e968f58961596872089a05b0d42f3fb2f103b450
rdass23 <85244928+rdass23@users.noreply.github.com>
2022-04-06 21:30:50 -0300

    Add files via upload

---
+++ A6/js/scripts.js
@@ -0,0 +1,9 @@
+function dropdown(){
+	var showNav = document.getElementById("myTopNav");
+	if (showNav.style.display === "none"){
+		showNav.style.display = "block";
+	}
+	else{
+		showNav.style.display = "none";
+	}
+}
